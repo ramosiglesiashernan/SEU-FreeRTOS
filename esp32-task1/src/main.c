6918861fb0a04f7c70150b1393c403eac6bc41cf
ramosiglesiashernan <ramosiglesiashernan@gmail.com>
2026-02-25 16:15:07 +0100

    Task1 completado

--- esp32-task1/src/main.c
+++ esp32-task1/src/main.c
@@ -9,36 +9,31 @@ static const char* TAG = "esp32-task1";
 void app_main() 
 {
   TaskHandle_t xHandle = NULL;
-  
-  /* Create a new task and add it to the list of tasks that are ready to run */
+
   xTaskCreate(
-      vTaskCode,                              /* Task function */
-      "Task",                                 /* Name of task; for human use */
-      configMINIMAL_STACK_SIZE*5,             /* Stack size of task */
-      NULL,                                   /* Parameter of the task */
-      CONFIG_ESP32_PTHREAD_TASK_PRIO_DEFAULT, /* Priority of the task */
-      &xHandle);                              /* Task handle to keep track of created task */
+      vTaskCode,
+      "Task",
+      configMINIMAL_STACK_SIZE*5,
+      NULL,
+      CONFIG_ESP32_PTHREAD_TASK_PRIO_DEFAULT,
+      &xHandle);
 
   configASSERT( xHandle );
 
-  /* Wait 1 sec */
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-  /* Use the handle to delete the task. */
   if (xHandle != NULL)
   {
-    vTaskDelete( xHandle );
+    vTaskDelete(xHandle);
   }  
 }
 
-/* Task function */
 void vTaskCode(void * parameter)
 {
   int counter = 0;
-  
-  /* loop forever */
+
   for(;;)
   {
-    ESP_LOGI(TAG, "Loop iteration %d\n", ++counter);
+    ESP_LOGI(TAG, "Loop iteration %d", ++counter);
   }
 }
